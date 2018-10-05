64b8686737ff92686bdf445cbf5b6cafc2746b4a
pavankumardasireddy <pavankumar.dasireddy@gmail.com>
2018-10-05 14:44:31 +0530

    Fixing unexpected commit issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ScrollToTop from 'react-scroll-up';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (
+        <div>
+            <App />
+            <ScrollToTop showUnder={160}>
+                <span title="Go To Top"><i className="scroll-top fa fa-arrow-up" aria-hidden="true" /></span>
+            </ScrollToTop>
+        </div>
+    ),document.getElementById('root')
+);
 registerServiceWorker();
